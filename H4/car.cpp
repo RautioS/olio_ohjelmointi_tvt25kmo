d8aa40d8666f2b0d80eb479666e694cc50f6f3e2
Santeri <santerirautio17@gmail.com>
2025-11-28 09:42:18 +0200

    H4 valmis

--- H4/car.cpp
+++ H4/car.cpp
@@ -24,12 +24,12 @@ void Car::setBrand(const string &newBrand)
 
 void Car::printDetails()
 {
-    std::cout << "Auto : " << brand << " " << model <<endl;
-    std::cout << "Moottori : " << objEngine.getHorsepower() << " hp, " << objEngine.getDisplacement() << " L" <<endl;
-    std::cout << "Rengas 1 : " << objWheel1.getSize() << " tuumaa, " << objWheel1.getType() << endl;
-    std::cout << "Rengas 2 : " << objWheel2.getSize() << " tuumaa, " << objWheel2.getType() << endl;
-    std::cout << "Rengas 3 : " << objWheel3.getSize() << " tuumaa, " << objWheel3.getType() << endl;
-    std::cout << "Rengas 4 : " << objWheel4.getSize() << " tuumaa, " << objWheel4.getType() << endl;
+    cout << "Auto : " << brand << " " << model <<endl;
+    cout << "Moottori : " << objEngine.getHorsepower() << " hp, " << objEngine.getDisplacement() << " L" <<endl;
+    cout << "Rengas 1 : " << objWheel1.getSize() << " tuumaa, " << objWheel1.getType() << endl;
+    cout << "Rengas 2 : " << objWheel2.getSize() << " tuumaa, " << objWheel2.getType() << endl;
+    cout << "Rengas 3 : " << objWheel3.getSize() << " tuumaa, " << objWheel3.getType() << endl;
+    cout << "Rengas 4 : " << objWheel4.getSize() << " tuumaa, " << objWheel4.getType() << endl;
 
 }
 
