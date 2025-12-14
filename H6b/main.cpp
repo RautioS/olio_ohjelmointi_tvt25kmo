cd91fd7d6ec40937524334e9bb87e9e8060e8b04
Santeri <santerirautio17@gmail.com>
2025-12-14 12:49:37 +0200

    Harjoitus 6a ja b valmiina

---
+++ H6b/main.cpp
@@ -0,0 +1,12 @@
+#include "exampleclass.h"
+
+#include <QCoreApplication>
+
+int main(int argc, char *argv[]) {
+    QCoreApplication a(argc, argv);
+
+    ExampleClass objExampleClass;
+    objExampleClass.startToWait();
+
+    return a.exec();
+}
